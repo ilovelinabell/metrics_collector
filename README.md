cee4bd3e1b33967a6b40a72d76e08f8254d08d12
bublyapplejuice2 <hth27@cornell.edu>
2023-08-05 22:07:41 -0400

    add cmd2 dependency
    grab env secrets, start container and login

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Server metrics collector inside of ClickHouse
 # Dependencies
 Python 3.9
 Poetry 1.5.1
-ClickHouse Connect API 0.2.6
+clickhouse-connect 0.6.8
+cmd2 2.4.3
 black
 isort
--- cli.py
+++ cli.py
@@ -1,1 +1,55 @@
 # An interactive command line interface for ClickHouse database through Python
+
+import argparse
+import os
+
+import clickhouse_connect
+from cmd2 import Cmd, Cmd2ArgumentParser, with_argparser
+
+
+class cli(Cmd):
+    def __init__():
+        super().__init__()
+
+
+argparser = Cmd2ArgumentParser()
+
+
+def Secrets():
+    host = os.getenv("CLICKHOUSE_HOST", "localhost")
+    storage_path = os.getenv("CLICKHOUSE_STORAGE_PATH", "/var/lib/clickhouse/")
+    log_path = os.getenv("CLICKHOUSE_LOG_PATH", "/var/log/clickhouse-server/")
+    return dict({"host": host, "storage_path": storage_path, "log_path": log_path})
+
+
+def main():
+    secrets = Secrets()
+
+    print(f'podman run --rm --network=host --user 1000:10\
+        -v {secrets.get("storage_path")}:/var/lib/clickhouse/ \
+        -v {secrets.get("log_path")}:/var/log/clickhouse-server/ \
+        clickhouse/clickhouse-server:latest')
+
+    ret = os.system(
+        f'podman run --rm --network=host --user 1000:10\
+        -v {secrets.get("storage_path")}:/var/lib/clickhouse/ \
+        -v {secrets.get("log_path")}:/var/log/clickhouse-server/ \
+        clickhouse/clickhouse-server:latest'
+    )
+
+    if ret:
+        raise RuntimeError("Error: could not start ClickHouse server")
+
+    metrics_collector = cli()
+
+    try:
+        client = clickhouse_connect.get_client(secrets.get("host"))
+    except:
+        print("WARNING: Could not connect to ClickHouse DB")
+    metrics_collector.intro = "Welcome to the ClickHouse Metrics Collector. Type help or ? to list commands.\n"
+    metrics_collector.prompt = "ClickHouseMetricsCollector> "
+    cli.cmdloop()
+
+
+if __name__ == "__main__":
+    main()
